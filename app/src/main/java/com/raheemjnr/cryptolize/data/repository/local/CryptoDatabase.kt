61097c6500e6062cbc45a75656dff7e0fb9adaae
Jnr <mumedian6@gmail.com>
2022-05-09 14:28:09 +0100

    Ui fixes( implementing offline caching with room  )

--- app/src/main/java/com/raheemjnr/cryptolize/data/repository/local/CryptoDatabase.kt
+++ app/src/main/java/com/raheemjnr/cryptolize/data/repository/local/CryptoDatabase.kt
@@ -1,2 +1,28 @@
 package com.raheemjnr.cryptolize.data.repository.local
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CryptoEntity::class], version = 1)
+abstract class CryptoDatabase() : RoomDatabase() {
+
+    //dao
+    abstract fun dao() : CryptoDao
+
+    companion object {
+        private var instance: CryptoDatabase? = null
+        private val LOCK = Any()
+
+        fun getInstance(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context.applicationContext).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                CryptoDatabase::class.java, "crypto.db"
+            ).build()
+    }
+}
--- app/src/main/java/com/raheemjnr/cryptolize/data/repository/local/Dao.kt
+++ app/src/main/java/com/raheemjnr/cryptolize/data/repository/local/Dao.kt
@@ -3,23 +3,26 @@ package com.raheemjnr.cryptolize.data.repository.local
 import androidx.room.*
 
 @Dao
-interface UserDao {
-    ¶
+interface CryptoDao {
+
     @Transaction
     @Query("SELECT * FROM local_crypto_table")
-    fun getAllCrypto(): List<CryptoEntity>
-
+    suspend fun getAllCrypto(): List<CryptoEntity>
 
     @Query(
         """
             SELECT * FROM local_crypto_table
-            WHERE LOWER(id)
+            WHERE LOWER(id) LIKE '%' || LOWER(:query) || '%' OR
+            UPPER(:query) == symbol
         """
     )
-   suspend fun searchCrypto(first: String, last: String): User
+    suspend fun searchCrypto(query: String): List<CryptoEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllCrypto(cryptoEntity: List<CryptoEntity>)
+
+    @Query("SELECT * FROM local_crypto_table")
+    suspend fun delete()
 
 
 }
