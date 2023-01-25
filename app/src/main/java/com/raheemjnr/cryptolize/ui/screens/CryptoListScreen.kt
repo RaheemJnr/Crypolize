a3b0255ffe9d96039843a95e035720f028aa0743
Raheem Jnr <mumedian6@gmail.com>
2023-01-25 06:35:54 +0100

    worked on pull refresh

--- app/src/main/java/com/raheemjnr/cryptolize/ui/screens/CryptoListScreen.kt
+++ app/src/main/java/com/raheemjnr/cryptolize/ui/screens/CryptoListScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -57,8 +58,8 @@ fun CryptoListScreen(navController: NavController) {
         topBar = {
             ListTopAppbar()
         }
-    ) {
-        Column {
+    ) { contentPadding ->
+        Column(Modifier.padding(contentPadding)) {
             ListCarousel {
                 context.openUrl(url = "https://github.com/RaheemJnr")
             }
