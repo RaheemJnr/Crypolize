9e1fad3d735cb50904560dfbbe11b851efab98ad
Raheem Jnr <mumedian6@gmail.com>
2023-04-24 18:10:39 +0100

    adding skimmer animation

--- app/src/main/java/com/raheemjnr/cryptolize/ui/components/CryptoListItems.kt
+++ app/src/main/java/com/raheemjnr/cryptolize/ui/components/CryptoListItems.kt
@@ -4,7 +4,15 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,33 +22,25 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.paging.LoadState
 import com.raheemjnr.cryptolize.data.repository.local.entity.CryptoEntity
-import com.raheemjnr.cryptolize.domain.models.Crypto
 import com.raheemjnr.cryptolize.utils.Formatter.formatCurrency
 import com.raheemjnr.cryptolize.utils.Formatter.roundPriceChange
 import com.raheemjnr.cryptolize.utils.Formatter.roundToThreeDecimals
 import com.raheemjnr.cryptolize.utils.Formatter.roundToTwoDecimals
-import com.raheemjnr.cryptolize.utils.SkeletonShimmerAnimation
-import java.util.*
+import java.util.Locale
 
 @Composable
 fun CryptoListItems(
     items: CryptoEntity?,
-    isLoading: Boolean = true,
     onClick: () -> Unit,
 ) {
     val animatedProgress = remember { Animatable(initialValue = 0.8f) }
@@ -95,137 +95,67 @@ fun CryptoListItems(
     ) {
         // name/pair
         Column {
-
-            SkeletonShimmerAnimation(
-                isLoading = isLoading,
-                shape = RectangleShape,
-                contentAlignment = Alignment.Center,
-                contentView = {
-                    Text(
-                        text = annotatedText
-                    )
-                },
-                defaultView = {
-                    Text(
-                        text = "                       "
-                    )
-                }
+            Text(
+                text = annotatedText
             )
+
             Spacer(modifier = Modifier.height(4.dp))
             //volume text
-            SkeletonShimmerAnimation(
-                isLoading = isLoading,
-                shape = RectangleShape,
-                contentAlignment = Alignment.Center,
-                contentView = {
-                    Text(
-                        text = "Vol ${
-                            items?.total_volume?.let {
-                                formatCurrency(
-                                    it.toInt()
-                                )
-                            }
-                        }",
-                        fontSize = 14.sp,
-                        color = MaterialTheme.colors.secondary
-                    )
-                },
-                defaultView = {
-                    Text(
-                        text = "       "
-                    )
-                }
+            Text(
+                text = "Vol ${
+                    items?.total_volume?.let {
+                        formatCurrency(
+                            it.toInt()
+                        )
+                    }
+                }",
+                fontSize = 14.sp,
+                color = MaterialTheme.colors.secondary
             )
         }
         // last price
         Column(
             modifier = Modifier.offset(x = (-20).dp)
         ) {
-            //
-            SkeletonShimmerAnimation(
-                isLoading = isLoading,
-                shape = RectangleShape,
-                contentAlignment = Alignment.Center,
-                contentView = {
-                    Text(
-                        text = formatCurrency(
-                            items?.current_price!!.roundToTwoDecimals().toDouble()
-                        ),
-                        fontSize = 18.sp,
-                        color = MaterialTheme.colors.primary
-                    )
-                },
-                defaultView = {
-                    Text(
-                        text = "       "
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            SkeletonShimmerAnimation(
-                isLoading = isLoading,
-                shape = RectangleShape,
-                contentAlignment = Alignment.Center,
-                contentView = {
-                    Text(
-                        text = formatCurrency(
-                            items?.current_price!!.roundToThreeDecimals().toDouble()
-                        ),
-                        fontSize = 14.sp,
-                        color = MaterialTheme.colors.secondary
-                    )
-                },
-                defaultView = {
-                    Text(
-                        text = "       "
-                    )
-                }
+            Text(
+                text = formatCurrency(
+                    items?.current_price!!.roundToTwoDecimals().toDouble()
+                ),
+                fontSize = 18.sp,
+                color = MaterialTheme.colors.primary
             )
 
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = formatCurrency(
+                    items.current_price.roundToThreeDecimals().toDouble()
+                ),
+                fontSize = 14.sp,
+                color = MaterialTheme.colors.secondary
+            )
         }
         // 24h change
-        SkeletonShimmerAnimation(
-            isLoading = isLoading,
-            shape = RectangleShape,
-            contentAlignment = Alignment.Center,
-            contentView = {
-                Surface(
-                    color = when {
-                        items?.price_change_percentage_24h!! > 0 -> Color(0xff32a852)
-                        items.price_change_percentage_24h.equals(0.0) -> Color.Gray
-                        else -> {
-                            Color.Red
-                        }
-                    },
-                    shape = RoundedCornerShape(4.dp),
-                    contentColor = Color.White,
-                    elevation = 8.dp,
-                ) {
-                    Text(
-                        text = "${items.price_change_percentage_24h.roundPriceChange()}%",
-                        style = MaterialTheme.typography.subtitle2,
-                        modifier = Modifier
-                            .width(55.dp)
-                            .height(35.dp)
-                            .padding(10.dp),
-                        textAlign = TextAlign.Center
-                    )
+        Surface(
+            color = when {
+                items?.price_change_percentage_24h!! > 0 -> Color(0xff32a852)
+                items.price_change_percentage_24h.equals(0.0) -> Color.Gray
+                else -> {
+                    Color.Red
                 }
             },
-            defaultView = {
-                Box(
-                    modifier = Modifier
-                        .width(55.dp)
-                        .height(35.dp)
-                        .clip(shape = RoundedCornerShape(4.dp))
-                        .padding(10.dp)
-
-
-                )
-
-            }
-        )
-
-
+            shape = RoundedCornerShape(4.dp),
+            contentColor = Color.White,
+            elevation = 8.dp,
+        ) {
+            Text(
+                text = "${items.price_change_percentage_24h.roundPriceChange()}%",
+                style = MaterialTheme.typography.subtitle2,
+                modifier = Modifier
+                    .width(55.dp)
+                    .height(35.dp)
+                    .padding(10.dp),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
